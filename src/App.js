dc1249630bf00c42b88b0887103d503a699f9e59
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-05 16:58:47 +0300

    created the home page for the login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import Navbar from './components/navbar';
+import AddPost from './components/postPage';
 function App() {
   const [user, setUser] = useState(localStorage.getItem('user'));
   if (!user)
@@ -12,16 +14,23 @@ function App() {
   else
     return (
       <div className="App">
-        <button
-          onClick={() => {
-            localStorage.removeItem('user');
-            setUser(false);
-          }}
-        >
-          Logout
-        </button>
+        <Navbar
+          elements={[
+            {
+              title: 'Logout',
+              active: false,
+              on_click: onLogout,
+            },
+          ]}
+        />
+        <AddPost />
       </div>
     );
+
+  function onLogout() {
+    localStorage.removeItem('user');
+    setUser(false);
+  }
 
   function updateUser() {
     setUser(true);
