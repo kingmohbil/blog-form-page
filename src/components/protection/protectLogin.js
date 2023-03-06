bc1d39ec6f6daff2bfd997f6599f8c5f55da54de
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-06 11:37:09 +0300

    The protection components for the routes are added in here

---
+++ src/components/protection/protectLogin.js
@@ -0,0 +1,8 @@
+import { Navigate } from 'react-router-dom';
+
+const Protect = ({ isLoggedIn, children }) => {
+  if (isLoggedIn) return <Navigate to="/" replace />;
+  return children;
+};
+
+export default Protect;
