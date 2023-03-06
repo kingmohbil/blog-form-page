a283504cb574038f7488533aaef719c3438ef86c
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-06 19:58:40 +0300

    Added the post component

--- src/components/post.js
+++ src/components/post.js
@@ -1,33 +1,58 @@
 import '../css/post.css';
+import CommentForm from './forms/commentForm';
 function Post(props) {
   return (
-    <div className="card" id="post">
-      <div className="card-body" id="post-body">
-        <div>{props.title}</div>
-        <button
-          className="btn btn-warning"
-          id="post-btn"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapseExample"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-          onClick={handleShowDescription}
-        >
-          Show Description
-        </button>
+    <>
+      <div className="col">
+        <div className="card h-100 text-dark bg-light mb-3">
+          <div className="card-header">
+            <div>
+              <h6 className="card-title">{props.username}</h6>
+              <h5 className="card-title">{props.title}</h5>
+            </div>
+            <small className="text-muted" id="footer-text">
+              {props.created_at}
+            </small>
+          </div>
+          <div className="card-body">
+            <p className="card-text">{props.message}</p>
+          </div>
+          <div className="card-footer">
+            <p id="comment-btn-container">
+              <button
+                className="btn btn-warning text-light"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={`#${props.postId}`}
+                aria-expanded="false"
+                aria-controls={props.postId}
+                onClick={handleShowComments}
+                id="comments-btn"
+              >
+                Add Comment
+              </button>
+            </p>
+            <div className="collapse" id={props.postId}>
+              <div
+                className="card card-body bg-secondary"
+                id="comments-container"
+              >
+                <CommentForm
+                  postId={props.postId}
+                  on_submit={props.on_submit}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body">{props.description}</div>
-      </div>
-    </div>
+    </>
   );
 }
 
 export default Post;
 
-function handleShowDescription(e) {
-  if (e.target.textContent === 'Hide Description')
-    e.target.textContent = 'Show Description';
-  else e.target.textContent = 'Hide Description';
+function handleShowComments(e) {
+  if (e.target.textContent === 'Close') e.target.textContent = 'Add Comments';
+  else e.target.textContent = 'Close';
 }
